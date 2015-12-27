9a8e2af8e4e59cc2fecf9608cdd35b4c50507ce6
Ray Tayek <ray@tayek.com>
2015-12-26 17:43:20 -0800

    rename main and fiddle build

--- src/main/java/com/tayek/tablet/Main2.java
+++ src/main/java/com/tayek/tablet/Main2.java
@@ -1,7 +1,7 @@
 package com.tayek.tablet;
 import java.io.*;
 import java.lang.reflect.InvocationTargetException;
-import com.tayek.tablet.Main;
+import com.tayek.tablet.Main2;
 import com.tayek.tablet.io.*;
 import com.tayek.tablet.io.swing.Swing;
 import static com.tayek.tablet.io.IO.*;
@@ -9,7 +9,7 @@ import com.tayek.utilities.*;
 // http://cs.nyu.edu/~yap/prog/cygwin/FAQs.html
 // http://poppopret.org/2013/01/07/suterusu-rootkit-inline-kernel-function-hooking-on-x86-and-arm/
 // http://angrytools.com/android/
-public class Main { // http://steveliles.github.io/invoking_processes_from_java.html
+public class Main2 { // http://steveliles.github.io/invoking_processes_from_java.html
     public static class Run2 {
         public static void main(String[] arguments)  {
             Swing.run2();
